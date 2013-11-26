5f380307a2d9a5988b4f03bcdecbca6d4484c6b4
Jake Spurlock <jspurlock@makermedia.com>
2013-11-26 14:58:45 -0800

    Getting there...

--- includes/shed/shed.php
+++ includes/shed/shed.php
@@ -22,7 +22,7 @@ function make_featured_products_slider() {
 
 	$products = $featured_products->Product;
 
-	$counter = range( 1, count( $products ) );
+	$counter = range( 0, ( count( $products ) - 1 ) );
 
 	shuffle( $counter );
 
@@ -32,8 +32,6 @@ function make_featured_products_slider() {
 	$output = '<div id="' . $id . '" class="carousel slide" data-interval="false"><div class="carousel-inner"><div class="row">';
 
 	foreach ( $counter as $key => $i ) {
-		var_dump( 'Key' . $key );
-		var_dump( 'Index ' . $i );
 		$output .= '<div class="span3 shed">';
 		$output .= '<a href="' . make_shed_url( $products[$i]->ProductCode ) . '"><img src="' . wpcom_vip_get_resized_remote_image_url( $products[$i]->PhotoURL, 218, 146 ) . '" alt="' . esc_attr( $products[$i]->Product_Name ) . '" /></a>';
 		$output .= '<h4><a href="';
@@ -47,21 +45,6 @@ function make_featured_products_slider() {
 			break;
 		}
 	}
-	// foreach ( $featured_products->Product as $product ) {
-	// 	$output .= '<div class="span3 shed">';
-	// 	$output .= '<a href="' . make_shed_url( $product->ProductCode ) . '"><img src="' . wpcom_vip_get_resized_remote_image_url( $product->PhotoURL, 218, 146 ) . '" alt="' . esc_attr( $product->Product_Name ) . '" /></a>';
-	// 	$output .= '<h4><a href="';
-	// 	$output .= make_shed_url( esc_attr( $product->ProductCode ) );
-	// 	$output .= '">';
-	// 	$output .= wp_kses_post( $product->ProductName );
-	// 	$output .= '</a></h4>';
-	// 	$output .= MarkDown( wp_trim_words( wp_kses_post( $product->ProductDescriptionShort ), 15, '...' ) );
-	// 	$output .= '</div>';
-	// 	if ( $i == 3 ) {
-	// 		break;
-	// 	}
-	// 	$i++;
-	// }
 	$output .= '</div></div></div>';
 	return $output;
 }
