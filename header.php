a64f120b125d033d916835f0b98f54e4a405bd40
Cole Geissinger <cgeissinger@makermedia.com>
2013-12-06 10:53:34 -0800

    Adding a title tag and alt tag because sir Jake said so.

--- header.php
+++ header.php
@@ -120,7 +120,7 @@
 				<div class="container">
 					<div class="row">
 						<div class="span12">
-							<a href="http://www.makershed.com/?Click=160268"><img src="http://makezineblog.files.wordpress.com/2013/12/maker-shed-banner-red.png" /></a>
+							<a href="http://www.makershed.com/?Click=160268" title="Find all your DIY electronics in the MakerShed. 3D Printing, Kits, Arduino, Raspberry Pi, Books &amp; more!"><img src="http://makezineblog.files.wordpress.com/2013/12/maker-shed-banner-red.png" alt="Find all your DIY electronics in the MakerShed. 3D Printing, Kits, Arduino, Raspberry Pi, Books &amp; more!" /></a>
 						</div>
 					</div>
 				</div>
