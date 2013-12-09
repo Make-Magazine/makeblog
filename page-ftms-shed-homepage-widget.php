55deca26f76a88e2a452d74854b1be678078ea30
Jake Spurlock <jspurlock@makermedia.com>
2013-12-09 13:34:18 -0800

    Some more tweaks for the shed ad.

--- page-ftms-shed-homepage-widget.php
+++ page-ftms-shed-homepage-widget.php
@@ -7,6 +7,7 @@
 <?php 
 
 header("Content-type: text/javascript");
+header('Content-Type: text/html; charset=utf-8');
 
 $args = array(
   'post_type' => 'from-the-maker-shed',
@@ -21,17 +22,12 @@ $wp_query = new WP_Query($args);
 
 <?php if( have_posts() ) : while ($wp_query->have_posts()) : $wp_query->the_post(); ?>
 
-var newcontent = document.createElement('p');
-newcontent.id = 'js-shed-homepage';
-newcontent.appendChild(document.write('
-	<div id="dotd">
-		<a href="<?php the_permalink(); ?>">
-			<?php if (class_exists('MultiPostThumbnails')) : MultiPostThumbnails::the_post_thumbnail('from-the-maker-shed', 'product-thumbnail', NULL, 'shed-thumb'); endif; ?>
-		</a>
-		<p><?php the_title(); ?></p>
-		<p><a href="<?php the_permalink(); ?>" class="cta">Learn more</a></p>
-	</div>'));
-var scr = document.getElementById('maker-shed-grabber');
-scr.parentNode.insertBefore(newcontent, scr);
+<?php $link = get_post_meta( get_the_id(), 'ftms_link', true ); ?>
+
+var newcontent = document.createElement('div');
+newcontent.id = 'maker-shed-grabber';
+newcontent.appendChild(document.write('<div id="dotd"><a href="<?php echo esc_url( $link ); ?>"><?php if (class_exists('MultiPostThumbnails')) : MultiPostThumbnails::the_post_thumbnail('from-the-maker-shed', 'product-thumbnail', NULL, 'shed-thumb'); endif; ?></a><p><?php the_title(); ?></p><p><a href="<?php echo esc_url( $link ); ?>" class="cta">Learn more</a></p></div>'));
+var src = document.getElementById('maker-shed-grabber');
+scr.parentNode.insertBefore(newcontent, src);
 
 <?php endwhile; endif; ?><!-- /loop -->
