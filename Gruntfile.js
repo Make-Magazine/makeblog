fcea5109b7d98a6f702f424b5db79869c39098fd
Cole Geissinger <cgeissinger@makermedia.com>
2014-01-10 15:50:48 -0800

    Moving to grunt!

---
+++ Gruntfile.js
@@ -0,0 +1,33 @@
+module.exports = function( grunt ) {
+
+	// All configurations go here
+	grunt.initConfig({
+
+		// Reads the package.json file
+		pkg: grunt.file.readJSON( 'package.json' ),
+
+		// Each Grunt plugins configurations will go here
+		less: {
+			development: {
+				files: {
+					'css/style.css': 'less/style.less',
+					'css/print.css': 'less/make/print.less'
+				}
+			}
+		},
+		watch: {
+			css: {
+				files: ['less/**/*.less'],
+				tasks: ['less']
+			}
+		}
+	});
+	
+	// Simplify the repetivite work of listing each plugin in grunt.loadNomTasks(), just get the list from package.json and load them...
+	require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+	// Tell Grunt to run these tasks by default.
+	// We can create new tasks with different names for specific tasks
+	grunt.registerTask( 'default', ['less', 'watch'] );
+
+};
