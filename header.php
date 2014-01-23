853aefc5eb4ca0ce1d74bbdee8870b750f731fd4
Cole Geissinger <cgeissinger@makermedia.com>
2014-01-23 12:18:31 -0800

    Removing blue bar navigation and adjusting the header layout

--- header.php
+++ header.php
@@ -33,21 +33,6 @@
 	</head>
 
 	<body <?php body_class(); ?>>
-
-		<div class="navbar navbar-fixed-top navbar-blue">
-			<div class="navbar-inner">
-				<div class="container">
-					<a onClick="_gaq.push([\'_trackEvent\', \'Links\', \'Click\', \'Home\']);" class="brand" href="<?php echo get_home_url(); ?>">MAKE</a>
-					<ul class="nav">
-						<li><a onClick="_gaq.push([\'_trackEvent\', \'Links\', \'Click\', \'Blog\']);" href="<?php echo home_url(); ?>/blog/">Blog</a></li>
-						<li><a onClick="_gaq.push([\'_trackEvent\', \'Links\', \'Click\', \'Magazine\']);" href="<?php echo home_url(); ?>/magazine/">Magazine</a></li>
-						<li><a onClick="_gaq.push([\'_trackEvent\', \'Links\', \'Click\', \'Maker Faire\']);" href="http://makerfaire.com">Maker Faire</a></li>
-						<li><a onClick="_gaq.push([\'_trackEvent\', \'Links\', \'Click\', \'Make: Projects\']);" href="<?php echo home_url(); ?>/projects">Make: Projects</a></li>
-						<li><a onClick="_gaq.push([\'_trackEvent\', \'Links\', \'Click\', \'Maker Shed\']);" href="http://makershed.com/">Maker Shed</a></li>
-					</ul>
-				</div>
-			</div>
-		</div>	
 		<div class="container hidden-print">
 			<div class="row">
 				<div id="div-gpt-ad-664089004995786621-1" class="text-center">
