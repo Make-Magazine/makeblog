af2bfc6c50badfe6001550157afa2cca659d4b60
Jake Spurlock <jspurlock@makermedia.com>
2014-01-27 17:09:56 -0800

    related posts...

--- includes/related.php
+++ includes/related.php
@@ -16,15 +16,29 @@ function make_get_related_content( $atts, $content = null ) {
 
 	$count = count( $ids );
 
-	$output = '<aside class="' . esc_attr( $class ) . '">';
+	$output = '<aside class="pull-quote ' . esc_attr( $class ) . ' pull-quote-' . esc_attr( $count ) . '"><h3><span>Related</span></h3><div class="row-fluid">';
 		if ( $count == 2 ) {
 			foreach ( $ids as $id ) {
+				$output .= '<div class="span6">';
 				$blurb = get_post( $id );
-				$output .= get_the_post_thumbnail( $blurb->ID, 'comment-thumb', array( 'class=' => 'pull-left' ) );
+				$output .= '<a href="' . get_permalink( $id ) . '" class="' . esc_attr( $class ) . '">';
+				$output .= get_the_post_thumbnail( $blurb->ID, 'comment-thumb', array( 'class' => 'pull-left' ) );
+				$output .= '</a>';
 				$output .= '<h4>' . apply_filters( 'the_title', $blurb->post_title ) . '</h4>';
 				$output .= Markdown( wp_trim_words( strip_shortcodes( $blurb->post_content ), 10, '...' ) );
 				wp_reset_postdata();
+				$output .= '</div>';
 			}
+		} elseif ( $count == 1 ) {
+			$output .= '<div class="span12">';
+				$blurb = get_post( $ids[0] );
+				$output .= '<a href="' . get_permalink( $ids[0] ) . '" class="' . esc_attr( $class ) . '">';
+				$output .= get_the_post_thumbnail( $blurb->ID, 'comment-thumb', array( 'class' => 'pull-left' ) );
+				$output .= '</a>';
+				$output .= '<h4>' . apply_filters( 'the_title', $blurb->post_title ) . '</h4>';
+				$output .= Markdown( wp_trim_words( strip_shortcodes( $blurb->post_content ), 10, '...' ) );
+				wp_reset_postdata();
+				$output .= '</div>';
 		}
 	$output .= '</aside>';
 	return $output;
