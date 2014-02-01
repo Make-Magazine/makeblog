8819ef518ce005cffda3b63addda07f0c5de10a6
David Beauchamp <dbeauchamp@makermedia.com>
2014-01-31 16:11:22 -0800

    Adding description field under titles for all post types and increasing their font from 18px to 20px and bold.

--- single-volume.php
+++ single-volume.php
@@ -17,13 +17,6 @@ get_header(); ?>
 				
 							<h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
 
-							<?php 
-								$desc = get_post_custom_values('Description');
-								if (isset($desc[0])) {
-									echo Markdown( wp_kses_post( $desc[0] ) );
-								}
-							?>
-
 							<div class="row">
 							
 								<div class="span8">
