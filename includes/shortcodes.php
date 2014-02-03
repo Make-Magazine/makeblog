3648477bd4389813ee4ba0e8951d02b49b6a2383
Bill Olson <bolson@makermedia.com>
2014-02-03 14:51:48 -0800

    Layout tweak for Galelio list

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -200,23 +200,15 @@ add_shortcode( 'gal', 'galileo_list' );
 function printrbot_sweepstakes_newsletter( $atts, $content = null ) {
 	return '
 		<form action="http://newsletter.makezine.com/t/r/s/tyilklk/" method="post">
-    		<p>
         		<label for="fieldName">Name</label>
         		<input id="fieldName" class="input-large" name="cm-name" type="text" />
-    		</p>
-    		<p>
         		<label for="fieldEmail">Email</label>
         		<input id="fieldEmail" class="input-large" name="cm-tyilklk-tyilklk" type="email" required />
-    		</p>
-    		<p>
         		<label>Opt into another list</label>
         		<input id="listjjuylk" name="cm-ol-jjuylk" type="checkbox" /> <label style="display:inline;" for="listjjuylk">MAKE Newsletter</label><br>
         		<input id="listjrsydu" name="cm-ol-jrsydu" type="checkbox" /> <label style="display:inline;" for="listjrsydu">Maker Pro Newsletter</label><br>
         		<input id="listttihir" name="cm-ol-ttihir" type="checkbox" /> <label style="display:inline;" for="listttihir">Maker Shed Newsletter</label><br>
-    		</p>
-    		<p>
         		<button class="btn blue btn-large" type="submit">Submit</button>
-    		</p>
 		</form>';
 }
 add_shortcode( 'printrbot', 'printrbot_sweepstakes_newsletter' );
