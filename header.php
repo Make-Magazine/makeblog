d0bdb97b664e70f8a37b04762d91bc46a3358ad3
David Beauchamp <dbeauchamp@makermedia.com>
2014-02-26 16:50:37 -0800

    Adding a Google Tag Manager script for the Shed to manage ads.

--- header.php
+++ header.php
@@ -30,9 +30,22 @@
 			</script>
 		<?php endif; ?>
 
+		<script type="text/javascript">
+			dataLayer = [];
+		</script>
 	</head>
 
 	<body <?php body_class(); ?>>
+		<!-- Google Tag Manager -->
+		<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-WR8NLB"
+		height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+		j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+		'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+		})(window,document,'script','dataLayer','GTM-WR8NLB');</script>
+		<!-- End Google Tag Manager -->
+		
 		<div class="container hidden-print">
 			<div class="row">
 				<div id="div-gpt-ad-664089004995786621-1" class="text-center">
