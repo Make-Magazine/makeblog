75dd1c5a825059aa2e000de8a7fdcad6d8d7d34b
Cole Geissinger <colegeissinger@gmail.com>
2014-03-10 14:48:15 -0700

    Beginning work on Backbone steps/parts/tools

--- page-contribute-project.php
+++ page-contribute-project.php
@@ -12,76 +12,72 @@ get_header(); ?>
 	
 		<div class="container authentication">
 
-			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-
 			<div class="row">
 			
 				<div class="span12">
-				
-					<article <?php post_class(); ?>>
 
-						<form class="form form-horizontal" method="post">
-							<?php echo wp_nonce_field( 'contribute_post' ); ?>
-							<fieldset>
-								<div class="control-group">
-									<div class="control-label"></div>
-									<div class="controls">
-										<div class="projects-masthead">
-											<h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+					<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+						<article <?php post_class(); ?>>
+
+							<form class="form form-horizontal" method="post">
+								<?php echo wp_nonce_field( 'contribute_post' ); ?>
+								<fieldset>
+									<div class="control-group">
+										<div class="control-label"></div>
+										<div class="controls">
+											<div class="projects-masthead">
+												<h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+											</div>
 										</div>
 									</div>
-								</div>
-								<div class="control-group">
-									<label class="control-label" for="post_title">Title</label>
-									<div class="controls">
-										<input type="text" class="input-xlarge" id="post_title">
-										<p class="help-block">Add the name of the post here.</p>
+									<div class="control-group">
+										<label class="control-label" for="post_title">Title</label>
+										<div class="controls">
+											<input type="text" class="input-xlarge" id="post_title">
+											<p class="help-block">Add the name of the post here.</p>
+										</div>
 									</div>
-								</div>
-								<div class="control-group">
-									<label class="control-label" for="post_content">Post Content</label>
-									<div class="controls">
-										<?php wp_editor( 'Tell us about your post...', 'post_content', array( 'teeny' => true ) ); ?>
+									<div class="control-group">
+										<label class="control-label" for="post_content">Post Content</label>
+										<div class="controls">
+											<?php wp_editor( 'Tell us about your post...', 'post_content', array( 'teeny' => true ) ); ?>
+										</div>
 									</div>
-								</div>
-								<div class="control-group">
-									<label class="control-label" for="category">Category</label>
-									<div class="controls">
-										<?php wp_dropdown_categories( array( 'hierarchical' => true, ) ); ?>
+									<div class="control-group">
+										<label class="control-label" for="category">Category</label>
+										<div class="controls">
+											<?php wp_dropdown_categories( array( 'hierarchical' => true, ) ); ?>
+										</div>
 									</div>
-								</div>
-								<div class="control-group">
-									<label class="control-label" for="post_featured_image">Add an Image</label>
-									<div class="controls">
-										<input class="input-file" id="post_featured_image" type="file">
+									<div class="control-group">
+										<label class="control-label" for="post_featured_image">Add an Image</label>
+										<div class="controls">
+											<input class="input-file" id="post_featured_image" type="file">
+										</div>
 									</div>
-								</div>
-								<div class="form-actions">
-									<button type="submit" class="btn btn-warning">Want to add steps to the post?</button>
-									<button type="submit" class="btn btn-primary submit-review">Submit for Review</button>
-								</div>
-								<section class="steps">
-									here are the steps.
-								</section>
-								<section class="parts">
-									Parts
-								</section>
-								<section class="tools">
-									here are the steps.
-								</section>
-								<div class="form-actions">
-									<button type="submit" class="btn btn-primary submit-review">Submit for Review</button>
-								</div>
-							</fieldset>
-						</form>
-					
-					</article>
-					
+									<div class="form-actions">
+										<button type="submit" class="btn btn-warning">Want to add steps to the post?</button>
+										<button type="submit" class="btn btn-primary submit-review">Submit for Review</button>
+									</div>
+									<section class="steps">
+										
+									</section>
+									<section class="parts"></section>
+									<section class="tools"></section>
+									<div class="form-actions">
+										<button type="submit" class="btn btn-primary submit-review">Submit for Review</button>
+									</div>
+								</fieldset>
+							</form>
+							
+						</article>
+
+					<?php endwhile; else: endif; ?>
+
 				<div>
 				
 			</div>
-			
-			<?php endwhile; else: endif; ?>
 			
 		</div>
 
