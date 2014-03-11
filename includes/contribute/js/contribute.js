384b562bf8342e395b2922816e8561847b16248e
Jake Spurlock <jspurlock@makermedia.com>
2014-03-11 15:57:09 -0700

    Start of steps and image uploads.

--- includes/contribute/js/contribute.js
+++ includes/contribute/js/contribute.js
@@ -37,6 +37,37 @@ jQuery( document ).ready( function( $ ) {
 		});
 	});
 
+	$( '.upload' ).on( 'click', function( e ) {
+
+		e.preventDefault();
+
+		var form = $('contribute-form');
+
+		var data = new FormData( form );
+		jQuery.each( $( '#file' )[0].files, function( i, file ) {
+			data.append( 'file-'+ i, file );
+		});
+
+		data.append( 'nonce',			$( '.contribute-form #contribute_post' ).val() );
+		data.append( 'post_parent',		$( '.contribute-form #post_title' ).val() );
+		data.append( 'action',			'upload_files' );
+
+		console.log( data );
+
+		$.ajax({
+			url: make_gigya.ajax,
+			data: data,
+			cache: false,
+			contentType: false,
+			processData: false,
+			type: 'POST',
+			success: function( data ){
+				post_obj = JSON.parse( data );
+				console.log( data );
+			}
+		});
+	});
+
 	$( '.submit-tools' ).on( 'click', function( e ) {
 
 		// Prevent the button from trggering
@@ -95,6 +126,40 @@ jQuery( document ).ready( function( $ ) {
 			}
 		});
 
+	});
+
+	// Handle the AJAX for saving the first stage of the post. The rest will be over Backbone.
+	$( '.submit-steps' ).on( 'click', function( e ) {
+
+		e.preventDefault();
+
+		var form = $('contribute-form-steps');
+
+		var data = new FormData( form );
+		jQuery.each( $( '#step-images' )[0].files, function( i, file ) {
+			data.append( 'step-images-'+ i, file );
+		});
+
+		data.append( 'nonce',			$( '.contribute-form #contribute_post' ).val() );
+		data.append( 'post_title',		$( '.contribute-form #post_title' ).val() );
+		data.append( 'post_content',	$( '.contribute-form #post_content' ).val() );
+		data.append( 'cat',				$( '.contribute-form #cat' ).val() );
+		data.append( 'action',			'contribute_post' );
+
+		console.log( data );
+
+		$.ajax({
+			url: make_gigya.ajax,
+			data: data,
+			cache: false,
+			contentType: false,
+			processData: false,
+			type: 'POST',
+			success: function( data ){
+				post_obj = JSON.parse( data );
+				console.log( data );
+			}
+		});
 	});
 
 });
