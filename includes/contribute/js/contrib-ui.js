f5f5cb14222335c1362dcec4141e7be4dd8946f3
Cole Geissinger <colegeissinger@gmail.com>
2014-03-13 14:47:34 -0700

    Still trying to get something done today...

--- includes/contribute/js/contrib-ui.js
+++ includes/contribute/js/contrib-ui.js
@@ -40,16 +40,51 @@ function make_contribute_remove_field( fields ) {
 	jQuery( '.btn.remove-' + field ).click( function( e ) {
 		e.preventDefault();
 
-		// Count the number of fields we have and decrement
-		var count = jQuery( 'input[name="total-' + fields + '"]' ).val();
-
 		// Remove the element
 		jQuery( this ).parents( '.' + field + '.row' ).remove();
 
-		// Update our field count
-		jQuery( 'input[name="total-' + fields + '"]' ).val( count - 1 );
-
 		// Make sure we reiterate over our steps and update their count. This will allow users to remove steps in-between steps
-		// make_contribute_update_fields( fields );
+		make_contribute_get_update_fields( fields );
 	});
+}
+
+
+function make_contribute_get_update_fields( fields ) {
+	console.log('Find what to update');
+	if ( fields === 'steps' ) {
+		make_contribute_update_steps();
+	} else if ( fields === 'parts' ) {
+		make_contribute_update_parts();
+	} else if ( fields === 'tools' ) {
+		make_contribute_update_tools();
+	}
+
+}
+
+
+function make_contribute_update_steps() {
+	console.log(jQuery('.step.row').length);
+	var i = 1;
+	jQuery( '.step.row' ).each( function() {
+		var step = jQuery(this);
+
+		// Update the step title
+		step.find( '.step-title-count' ).html( 'Step ' + i );
+
+		// Update the step image
+		i++;
+		console.log(i);
+	});
+
+	// Update the total step count
+	jQuery( '.contribute-form-steps' ).find( 'input[name="total-steps"]' ).val( i );
+	console.log('Done');
+}
+
+function make_contribute_update_parts() {
+
+}
+
+function make_contribute_update_tools() {
+
 }
