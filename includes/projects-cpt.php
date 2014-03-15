b6538c3d32d47f3134b46dbbae3f3794e34bf602
Jake Spurlock <jspurlock@makermedia.com>
2014-03-14 23:42:51 -0700

    If there is only one step, just output the markdowned text, instead of a UL

--- includes/projects-cpt.php
+++ includes/projects-cpt.php
@@ -576,6 +576,14 @@ function make_projects_steps( $steps, $print = false ) {
 			echo '<ul>';
 			foreach ($lines as $line) {
 				echo '<li>' . wp_kses_post( stripslashes( $line->text ) ) . '</li>';
+			if ( isset( $lines[1] ) ) {
+				echo '<ul>';
+				foreach ($lines as $line) {
+					echo '<li>' . wp_kses_post( stripslashes( $line->text ) ) . '</li>';
+				}
+				echo '</ul>';
+			} else {
+				echo Markdown( wp_kses_post( $lines[0]->text ) );
 			}
 			echo '</ul>';
 			echo '</div><!--.right_column-->';
