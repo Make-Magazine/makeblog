2a6360d95ff8ecc43e6fc4c69fec4c366b87836d
Cole Geissinger <colegeissinger@gmail.com>
2014-03-20 16:14:39 -0700

    Removing infinite scroll from archive pages

--- includes/theme_stuff.php
+++ includes/theme_stuff.php
@@ -84,7 +84,7 @@ add_action( 'after_setup_theme', 'make_action_after_setup_theme' );
  * @since P-body
  */
 function make_adjust_infinite_scroll_support() {
-	$supported = current_theme_supports( 'infinite-scroll' ) && ( is_home() || is_archive() || is_author() );
+	$supported = current_theme_supports( 'infinite-scroll' ) && ( is_home() || is_author() );
 
 	return $supported;
 }
