431db818e5a5920d0cfe2c0148bcaf92e1947109
Jake Spurlock <jspurlock@makermedia.com>
2014-04-14 17:12:16 -0700

    Dropping the random s...

--- includes/class-author-profile.php
+++ includes/class-author-profile.php
@@ -111,7 +111,7 @@
 						$output .= '<ul class="clearfix inline">';
 						$output .= ( !empty( $author->user_email ) ) ? '<li><a href="' . esc_url( antispambot( "mailto:" . $author->user_email ) ) . '">' . antispambot( $author->user_email ) . '</a></li>' : '' ;
 						$output .= ( !empty( $author->user_email ) && !empty( $author->website ) ) ? ' <li>//</li>' : '' ;
-						$output .= ( !empty( $author->website ) ) ? '<li><a href="' . esc_url( $author->website ) . '">Website</a></li>' : 's' ;
+						$output .= ( !empty( $author->website ) ) ? '<li><a href="' . esc_url( $author->website ) . '">Website</a></li>' : '' ;
 						$output .= '</ul>';
 					} else {
 						$output .= $this->author_bio( $author_meta );
