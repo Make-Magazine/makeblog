f4dbf9c84f39576ffd5a2818c0b008e4f81a54a7
Jake Spurlock <jspurlock@makermedia.com>
2014-05-14 14:25:15 -0700

    Show off.

--- home-faire-instagram.php
+++ home-faire-instagram.php
@@ -4,5 +4,6 @@
  */
 ?>
 <div class="row-fluid">
+	<h2 class="red"><a href="http://instagram.com/makerfaire" title="Maker Faire on Instagram">@makerfaire</a> on Instagram</h2>
 	<?php echo make_show_images(); ?>
 </div>
