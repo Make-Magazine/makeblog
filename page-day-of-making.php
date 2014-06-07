2093d7f4e66a75e63d8f1df9ae891ce1f6a6e33a
Jake Spurlock <jspurlock@makermedia.com>
2014-06-06 17:35:16 -0700

    Link up the modal to the button.

--- page-day-of-making.php
+++ page-day-of-making.php
@@ -51,7 +51,7 @@
 
 				<p>Tongue voluptate occaecat eu turducken nostrud incididunt meatball ut spare ribs qui. Ut id jowl pork loin jerky. Short ribs brisket anim sirloin voluptate, laboris dolore ex id beef ribs. Meatball duis prosciutto pork ut. Irure beef nostrud sausage.</p>
 
-				<a class="btn btn-large btn-block" href="#" title="Call to Action" data-toggle="modal" href="#myModal">Call to Action</a>
+				<a role="button" data-toggle="modal" class="btn btn-large btn-block" title="Call to Action" data-toggle="modal" href="#myModal">Call to Action</a>
 
 			</div>
 
@@ -279,131 +279,126 @@
 
 	</section>
 
-	<div class="modal" id="myModal">
+	<div class="modal hide fade" id="myModal">
 		<div class="modal-header">
-			<a class="close" data-dismiss="modal">&times;</a>
+			<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
 			<h3>Register for the Day of Making</h3>
 		</div>
 		<div class="modal-body">
 			<form class="form-horizontal" id="day-of-making-form">
-				<form class="form-horizontal">
-					<fieldset>
-
-					<!-- Text input-->
-					<div class="control-group">
-						<label class="control-label" for="firstname">Full Name</label>
-						<div class="controls">
-					    	<input id="firstname" name="firstname" type="text" placeholder="" class="input-medium" required="">
-					    	<input id="firstname" name="lastname" type="text" placeholder="" class="input-medium" required="">
-						</div>
+				<!-- Text input-->
+				<div class="control-group">
+					<label class="control-label" for="firstname">Full Name</label>
+					<div class="controls">
+				    	<input id="firstname" name="firstname" type="text" placeholder="" class="input-medium" required="">
+				    	<input id="firstname" name="lastname" type="text" placeholder="" class="input-medium" required="">
 					</div>
+				</div>
 
-					<!-- Text input-->
-					<div class="control-group">
-						<label class="control-label" for="city">City</label>
-						<div class="controls">
-							<input id="city" name="city" type="text" placeholder="" class="input-medium" required="">
-							<select id="state" name="state" class="input-medium">
-								<option value="" selected="selected">Select a State</option>
-								<option value="AL">Alabama</option>
-								<option value="AK">Alaska</option>
-								<option value="AZ">Arizona</option>
-								<option value="AR">Arkansas</option>
-								<option value="CA">California</option>
-								<option value="CO">Colorado</option>
-								<option value="CT">Connecticut</option>
-								<option value="DE">Delaware</option>
-								<option value="DC">District Of Columbia</option>
-								<option value="FL">Florida</option>
-								<option value="GA">Georgia</option>
-								<option value="HI">Hawaii</option>
-								<option value="ID">Idaho</option>
-								<option value="IL">Illinois</option>
-								<option value="IN">Indiana</option>
-								<option value="IA">Iowa</option>
-								<option value="KS">Kansas</option>
-								<option value="KY">Kentucky</option>
-								<option value="LA">Louisiana</option>
-								<option value="ME">Maine</option>
-								<option value="MD">Maryland</option>
-								<option value="MA">Massachusetts</option>
-								<option value="MI">Michigan</option>
-								<option value="MN">Minnesota</option>
-								<option value="MS">Mississippi</option>
-								<option value="MO">Missouri</option>
-								<option value="MT">Montana</option>
-								<option value="NE">Nebraska</option>
-								<option value="NV">Nevada</option>
-								<option value="NH">New Hampshire</option>
-								<option value="NJ">New Jersey</option>
-								<option value="NM">New Mexico</option>
-								<option value="NY">New York</option>
-								<option value="NC">North Carolina</option>
-								<option value="ND">North Dakota</option>
-								<option value="OH">Ohio</option>
-								<option value="OK">Oklahoma</option>
-								<option value="OR">Oregon</option>
-								<option value="PA">Pennsylvania</option>
-								<option value="RI">Rhode Island</option>
-								<option value="SC">South Carolina</option>
-								<option value="SD">South Dakota</option>
-								<option value="TN">Tennessee</option>
-								<option value="TX">Texas</option>
-								<option value="UT">Utah</option>
-								<option value="VT">Vermont</option>
-								<option value="VA">Virginia</option>
-								<option value="WA">Washington</option>
-								<option value="WV">West Virginia</option>
-								<option value="WI">Wisconsin</option>
-								<option value="WY">Wyoming</option>
-								</select>
+				<!-- Text input-->
+				<div class="control-group">
+					<label class="control-label" for="city">City</label>
+					<div class="controls">
+						<input id="city" name="city" type="text" placeholder="" class="input-medium" required="">
+						<select id="state" name="state" class="input-medium">
+							<option value="" selected="selected">Select a State</option>
+							<option value="AL">Alabama</option>
+							<option value="AK">Alaska</option>
+							<option value="AZ">Arizona</option>
+							<option value="AR">Arkansas</option>
+							<option value="CA">California</option>
+							<option value="CO">Colorado</option>
+							<option value="CT">Connecticut</option>
+							<option value="DE">Delaware</option>
+							<option value="DC">District Of Columbia</option>
+							<option value="FL">Florida</option>
+							<option value="GA">Georgia</option>
+							<option value="HI">Hawaii</option>
+							<option value="ID">Idaho</option>
+							<option value="IL">Illinois</option>
+							<option value="IN">Indiana</option>
+							<option value="IA">Iowa</option>
+							<option value="KS">Kansas</option>
+							<option value="KY">Kentucky</option>
+							<option value="LA">Louisiana</option>
+							<option value="ME">Maine</option>
+							<option value="MD">Maryland</option>
+							<option value="MA">Massachusetts</option>
+							<option value="MI">Michigan</option>
+							<option value="MN">Minnesota</option>
+							<option value="MS">Mississippi</option>
+							<option value="MO">Missouri</option>
+							<option value="MT">Montana</option>
+							<option value="NE">Nebraska</option>
+							<option value="NV">Nevada</option>
+							<option value="NH">New Hampshire</option>
+							<option value="NJ">New Jersey</option>
+							<option value="NM">New Mexico</option>
+							<option value="NY">New York</option>
+							<option value="NC">North Carolina</option>
+							<option value="ND">North Dakota</option>
+							<option value="OH">Ohio</option>
+							<option value="OK">Oklahoma</option>
+							<option value="OR">Oregon</option>
+							<option value="PA">Pennsylvania</option>
+							<option value="RI">Rhode Island</option>
+							<option value="SC">South Carolina</option>
+							<option value="SD">South Dakota</option>
+							<option value="TN">Tennessee</option>
+							<option value="TX">Texas</option>
+							<option value="UT">Utah</option>
+							<option value="VT">Vermont</option>
+							<option value="VA">Virginia</option>
+							<option value="WA">Washington</option>
+							<option value="WV">West Virginia</option>
+							<option value="WI">Wisconsin</option>
+							<option value="WY">Wyoming</option>
 							</select>
-					  </div>
-					</div>
+						</select>
+				  </div>
+				</div>
 
-					<!-- Text input-->
-					<div class="control-group">
-						<label class="control-label" for="email_address">Email Address</label>
-						<div class="controls">
-					    	<input id="email_address" name="email_address" type="email" placeholder="" class="input-xlarge" required="">
-					    	<p class="help-block"><small><em>We use Gravatar for the images. Don't see yours? Try tying to an account here: <a href="http://en.gravatar.com">http://en.gravatar.com</a></em><small></p>
-						</div>
+				<!-- Text input-->
+				<div class="control-group">
+					<label class="control-label" for="email_address">Email Address</label>
+					<div class="controls">
+				    	<input id="email_address" name="email_address" type="email" placeholder="" class="input-xlarge" required="">
+				    	<p class="help-block"><small><em>We use Gravatar for the images. Don't see yours? Try tying to an account here: <a href="http://en.gravatar.com">http://en.gravatar.com</a></em></small></p>
 					</div>
+				</div>
 
-					<!-- Select Basic -->
-					<div class="control-group">
-					  <label class="control-label" for="experience">Experience Level</label>
-					  <div class="controls">
-					    <select id="experience" name="experience" class="input-medium">
-					      <option>Goldsmith</option>
-					      <option>Coppersmith</option>
-					    </select>
-					  </div>
+				<!-- Select Basic -->
+				<div class="control-group">
+				  <label class="control-label" for="experience">Experience Level</label>
+				  <div class="controls">
+				    <select id="experience" name="experience" class="input-medium">
+				      <option>Goldsmith</option>
+				      <option>Coppersmith</option>
+				    </select>
+				  </div>
+				</div>
+
+				<!-- Select Basic -->
+				<div class="control-group">
+					<label class="control-label" for="category">Category</label>
+					<div class="controls">
+						<?php wp_dropdown_categories( array( 'class' => 'input-medium' ) ); ?>
 					</div>
+				</div>
 
-					<!-- Select Basic -->
-					<div class="control-group">
-						<label class="control-label" for="category">Category</label>
-						<div class="controls">
-							<?php wp_dropdown_categories( array( 'class' => 'input-medium' ) ); ?>
-						</div>
+				<!-- Textarea -->
+				<div class="control-group">
+					<label class="control-label" for="post_content">What I make:</label>
+					<div class="controls">
+				    	<textarea id="post_content" class="input-xlarge" rows="3" name="post_content"></textarea>
 					</div>
-
-					<!-- Textarea -->
-					<div class="control-group">
-						<label class="control-label" for="post_content">What I make:</label>
-						<div class="controls">
-					    	<textarea id="post_content" class="input-xlarge" rows="3" name="post_content"></textarea>
-						</div>
-					</div>
-
-					</fieldset>
-					</form>
+				</div>
 
 		</div>
 		<div class="modal-footer">
-			<a href="#" class="btn btn-primary">Save changes</a>
-			<a href="#" class="btn">Close</a>
+			</form>
+
+			<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
 		</div>
 	</div>
 
