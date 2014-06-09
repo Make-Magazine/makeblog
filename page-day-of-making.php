b8e2b120faf97b41ecc6419ba9badd377344c4ee
Jake Spurlock <jspurlock@makermedia.com>
2014-06-09 08:22:09 -0700

    Late escape.

--- page-day-of-making.php
+++ page-day-of-making.php
@@ -7,7 +7,7 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title><?php echo make_generate_title_tag(); ?></title>
+	<title><?php echo esc_attr( make_generate_title_tag() ); ?></title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="description" content="<?php esc_attr( the_excerpt() ); ?>">
 
