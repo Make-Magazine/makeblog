1c0d7ced283cec4293aae34a3bb8374ea34b791c
Jake Spurlock <jspurlock@makermedia.com>
2014-06-09 16:29:51 -0700

    Add a class to the gravatar.

--- includes/post-types/makers.php
+++ includes/post-types/makers.php
@@ -141,6 +141,11 @@ class Make_Makers {
 
 	}
 
+	public function change_avatar_css($class) {
+		$class = str_replace( "class='avatar", "class='avatar pull-left ", $class ) ;
+		return $class;
+	}
+
 
 	/**
 	 * Build the output of the rows
