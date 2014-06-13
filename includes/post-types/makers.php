db60805b9af7e2777dbc8a3dd5e53941e9657986
Jake Spurlock <jspurlock@makermedia.com>
2014-06-13 12:03:45 -0700

    Add a default for the class.

--- includes/post-types/makers.php
+++ includes/post-types/makers.php
@@ -147,7 +147,7 @@ class Make_Makers {
 
 	}
 
-	private function build_avatar( $url, $size = 120, $class ) {
+	private function build_avatar( $url, $size = 120, $class = '' ) {
 		return '<img src="' . esc_url( wpcom_vip_get_resized_remote_image_url( $url, $size, $size ) ) . '" class="avatar avatar-' . esc_attr( $size ) . ' photo ' . $class . '">';
 	}
 
