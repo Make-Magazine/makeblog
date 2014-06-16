b82ccc61754e84b28338e229098cbcc46ebc892d
Jake Spurlock <jspurlock@makermedia.com>
2014-06-16 13:37:07 -0700

    Drop the width form the buttons.

--- page-day-of-making.php
+++ page-day-of-making.php
@@ -304,7 +304,7 @@
 
 					<div class="map-holder"></div>
 
-					<p class="stars"><a role="button" data-toggle="modal" class="btn btn-danger btn-large" style="width:220px;" title="Join other makers" data-toggle="modal" href="#join">Sign the "Building Maker Communities" pledge</a></p>
+					<p class="stars"><a role="button" data-toggle="modal" class="btn btn-danger btn-large" title="Join other makers" data-toggle="modal" href="#join">Sign the "Building Maker Communities" pledge</a></p>
 
 					<p class="stars"><img src="<?php echo esc_url( get_stylesheet_directory_uri() . '/img/stars.png' ); ?>" alt="Stars"></p>
 
@@ -437,7 +437,7 @@
 
 					<p>We are working together to build maker communities throughout America and the world that introduce the maker movement to more people and expand the benefits that makers can realize through their participation. Thank you for your recognition of the maker community and its strategic importance for our future.  We look forward to working with you and your Administration to make this effort a huge success.</p>
 
-					<p class="stars"><a role="button" data-toggle="modal" class="btn btn-danger btn-large" style="width:220px;" title="Join other makers" data-toggle="modal" href="#join">Sign the "Building Maker Communities" pledge</a></p>
+					<p class="stars"><a role="button" data-toggle="modal" class="btn btn-danger btn-large" title="Join other makers" data-toggle="modal" href="#join">Sign the "Building Maker Communities" pledge</a></p>
 
 					<p class="stars"><img src="<?php echo esc_url( get_stylesheet_directory_uri() . '/img/stars.png' ); ?>" alt="Stars"></p>
 
