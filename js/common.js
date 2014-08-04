57655aa40d36555c21477ae6e772dd2b402b7374
Bill Olson <bolson@makermedia.com>
2014-08-04 09:54:03 -0700

    Google Universal Analytics Upgrade

--- js/common.js
+++ js/common.js
@@ -6,22 +6,23 @@
 try{Typekit.load();}catch(e){}
 
 
-// Load Google Analytics
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-51157-1']);
-_gaq.push(['_trackPageview']);
+<!-- Google Analytics -->
+<script>
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-(function() {
-	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
+ga('create', 'UA-51157-1', 'auto');  // Replace with your property ID.
+ga('send', 'pageview');
 
+</script>
+<!-- End Google Analytics -->
 
 jQuery( document ).ready( function( $ ) {
     // Sadly the Facebook Comment Box does not allow us to change the positioning
     $( '.comment-list' ).appendTo( '#comments' );
-    
+
     // Allow links within bootstrap tabs for sharing URL of a particular tab
     var url = document.location.toString();
     if ( url.match( '#' ) ) {
