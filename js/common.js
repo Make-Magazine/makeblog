ef45ec2de53d80f90b238ca47d0e4974afe539ab
Bill Olson <bolson@makermedia.com>
2014-08-11 15:43:55 -0700

    Universal Analytics

--- js/common.js
+++ js/common.js
@@ -5,18 +5,14 @@
 // Load Typekit
 try{Typekit.load();}catch(e){}
 
-
 <!-- Google Analytics -->
-<script>
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-51157-1', 'auto');  // Replace with your property ID.
+ga('create', 'UA-51157-1', 'auto');  //  Property ID.
 ga('send', 'pageview');
-
-</script>
 <!-- End Google Analytics -->
 
 jQuery( document ).ready( function( $ ) {
