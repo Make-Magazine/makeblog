ecf3cfc147f48a29d6853752b41d9c0af00e6077
Bill Olson <bolson@makermedia.com>
2014-08-13 15:33:23 -0700

    Transfer Google Analytics

--- js/common.js
+++ js/common.js
@@ -5,16 +5,6 @@
 // Load Typekit
 try{Typekit.load();}catch(e){}
 
-<!-- Google Analytics -->
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-ga('create', 'UA-51157-1', 'auto');  //  Property ID.
-ga('send', 'pageview');
-<!-- End Google Analytics -->
-
 jQuery( document ).ready( function( $ ) {
     // Sadly the Facebook Comment Box does not allow us to change the positioning
     $( '.comment-list' ).appendTo( '#comments' );
