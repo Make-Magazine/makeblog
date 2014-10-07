76ea581dc98f7caba28e0858bf097b22d8855300
Bill Olson <bolson@makermedia.com>
2014-10-07 10:38:57 -0700

    Universal Analytics update

--- page-header-subscribe.php
+++ page-header-subscribe.php
@@ -30,17 +30,15 @@ Template name: Header
 		<script type="text/javascript" src="https://use.typekit.com/fzm8sgx.js"></script>
 		<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
 		<script type="text/javascript">
+			  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-		var _gaq = _gaq || [];
-		_gaq.push(['_setAccount', 'UA-51157-1']);
-		_gaq.push(['_trackPageview']);
-
-		(function() {
-			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-		})();
-
+			  ga('create', 'UA-51157-1', 'auto');
+			  ga('send', 'pageview', {
+			 'page': location.pathname + location.search  + location.hash
+			  });
 		</script>
 
 		<?php // Since this loads into https://readerservices.makezine.com, the get_template_directory_uri() fails to load https, so we need to hard code. ?>
