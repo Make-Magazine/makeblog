65ce4f83feae9d7f89e8d58974b0b2ae7cf6e5c7
Bill Olson <bolson@makermedia.com>
2014-10-22 16:52:00 -0700

    Re-enabling infinite scroll

--- home.php
+++ home.php
@@ -62,13 +62,6 @@ get_header(); ?>
 
 					<?php endif; ?>
 
-					<ul class="pager">
-
-						<li class="previous"><?php previous_posts_link('&larr; Previous Page'); ?></li>
-						<li class="next"><?php next_posts_link('Next Page &rarr;'); ?></li>
-
-					</ul>
-
 				</div>
 
 
