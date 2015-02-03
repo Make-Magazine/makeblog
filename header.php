687a086b86e3a6cd36475165549c46d818cdcd72
Bill Olson <bolson@makermedia.com>
2015-02-02 19:26:35 -0800

    Navigation menu update

--- header.php
+++ header.php
@@ -121,16 +121,7 @@
 							<?php endif; ?>
 						</div>
 						<nav role="navigation" class="span7 site-navigation primary-navigation hidden-print">
-							<?php
-								wp_nav_menu( array(
-									'theme_location'  => 'make-primary',
-									'container'       => false,
-									'menu_class'      => 'nav menu-primary-nav ga-nav clearfix',
-								) );
-							?>
 						</nav>
-
-
 						<div class="additional-content hidden-print">
 							<form action="<?php echo home_url(); ?>" class="search-make open">
 								<input type="text" class="search-field" name="s" placeholder="Search" />
@@ -138,7 +129,7 @@
 							</form>
 							<div class="clearfix"></div>
 							<div class="hdr-sub-ad-01" >
-								<a href="https://www.pubservice.com/MK/subscribe.aspx?PC=MK&amp;PK=M3BMZA"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/img/hdr-mag-sub-01.jpg"  alt="Subscribe to Make Magazine Today!" /></a>
+								<a href="https://www.pubservice.com/MK/subscribe.aspx?PC=MK&amp;PK=M3BMZA"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/img/hdr-mag-sub-43.jpg"  alt="Subscribe to Make Magazine Today!" /></a>
 							</div>	
 						</div>
 					</div>
@@ -148,13 +139,139 @@
 				<div class="container">
 					<div class="row">
 						<nav class="span12 site-navigation secondary-navigation">
-							<?php
-								wp_nav_menu( array(
-									'theme_location' => 'make-secondary',
-									'container'		 => false,
-									'menu_class' 	 => 'nav menu-secondary-nav ga-nav clearfix',
-								) );
-							?>
+							<ul id="menu-make-secondary-nav" class="nav navbar-nav ga-nav clearfix">
+								<li class="mega-box dropdown"><a href="https://makezine.com/projects/?path=FromNav" class="dropdown-toggle">Projects</a>
+									<ul class="sub-menu dropdown-menu container dropdown" style="width:940px;">
+										<div class="span2">
+											<ul class="mega-dropdown">
+												<li class="top-cat-item"><a href="https://makezine.com/category/electronics/?path=FromNav">Electronics</a></li>
+												<li><a href="https://makezine.com/category/electronics/arduino/?post_type=projects&amp;path=FromNav">Arduino</a></li>
+												<li><a href="https://makezine.com/category/electronics/computers-mobile/?post_type=projects&amp;path=FromNav">Computers &amp; Mobile</a></li>
+												<li><a href="https://makezine.com/category/electronics/raspberry-pi/?post_type=projects&amp;path=FromNav">Raspberry Pi</a></li>
+												<li><a href="https://makezine.com/category/electronics/robotics/?post_type=projects&amp;path=FromNav">Robotics</a></li>
+												<li class="browse-all"><a href="http://makezine.com/category/electronics/?path=FromNav">❯ Browse All</a></li>
+											</ul>
+										</div>
+										<div class="span2">
+											<ul class="mega-dropdown">
+												<li class="top-cat-item"><a href="https://makezine.com/category/workshop/?path=FromNav">Workshop</a></li>
+												<li><a href="https://makezine.com/category/workshop/3d-printing-workshop/?path=FromNav">3D Printing</a></li>
+												<li><a href="https://makezine.com/category/workshop/cnc-machining/?path=FromNav">CNC Machining</a></li>
+												<li><a href="https://makezine.com/category/workshop/computer-controlled-cutting/?path=FromNav">Computer-Controlled Cutting</a></li>
+												<li><a href="https://makezine.com/category/workshop/machining/?path=FromNav">Machining</a></li>
+												<li><a href="https://makezine.com/category/workshop/tools/?path=FromNav">Tools</a></li>
+												<li><a href="https://makezine.com/category/workshop/woodworking/?path=FromNav">Woodworking</a></li>
+												<li class="browse-all"><a href="https://makezine.com/category/workshop/?path=FromNav">❯ Browse All</a></li>
+											</ul>
+
+										</div>
+										<div class="span2">
+											<ul class="mega-dropdown">
+												<li class="top-cat-item"><a href="http://makezine.com/craftzine/?path=FromNav">Craft</a></li>
+												<li><a href="https://makezine.com/category/craft/crochet/?path=FromNav">Crochet</a></li>
+												<li><a href="https://makezine.com/category/craft/knitting/?path=FromNav">Knitting</a></li>
+												<li><a href="https://makezine.com/category/craft/paper-crafts/?path=FromNav">Paper Crafts</a></li>
+												<li><a href="https://makezine.com/category/craft/sewing-craft/?path=FromNav">Sewing</a></li>
+												<li class="browse-all"><a href="http://makezine.com/craftzine/?path=FromNav">❯ Browse All</a></li>
+											</ul>
+										</div>
+										<div class="span2">
+
+											<ul class="mega-dropdown">
+												<li class="top-cat-item"><a href="http://makezine.com/category/science/?path=FromNav">Science</a></li>
+												<li><a href="https://makezine.com/category/science/energy/?post_type=projects&amp;path=FromNav">Energy</a></li>
+												<li><a href="https://makezine.com/category/science/health-science/?post_type=projects&amp;path=FromNav">Health</a></li>
+												<li class="browse-all"><a href="http://makezine.com/category/science/?path=FromNav">❯ Browse All</a></li>
+											</ul>
+										</div>
+										<div class="span2">
+											<ul class="mega-dropdown">
+												<li class="top-cat-item"><a href="https://makezine.com/category/home/?path=FromNav">Home</a></li>
+												<li><a href="https://makezine.com/category/home/food-beverage/?post_type=projects&amp;path=FromNav">Food &amp; Beverage</a></li>
+												<li><a href="https://makezine.com/category/home/fun-games/?post_type=projects&amp;path=FromNav">Fun &amp; Games</a></li>
+												<li><a href="https://makezine.com/category/home/furniture/?post_type=projects&amp;path=FromNav">Furniture</a></li>
+												<li><a href="https://makezine.com/category/home/gardening/?post_type=projects&amp;path=FromNav">Gardening</a></li>
+												<li><a href="https://makezine.com/category/home/hacks/?post_type=projects&amp;path=FromNav">Hacks</a></li>
+												<li><a href="https://makezine.com/category/home/kids-family/?post_type=projects&amp;path=FromNav">Kids &amp; Family</a></li>
+												<li class="browse-all"><a href="https://makezine.com/category/home/?path=FromNav">❯ Browse All</a></li>
+											</ul>
+										</div>
+										<div class="span2">
+											<ul class="mega-dropdown">
+												<li class="top-cat-item"><a href="http://makezine.com/category/art-design/?path=FromNav">Art &amp; Design</a></li>
+												<li><a href="https://makezine.com/category/art-design/architecture-art-design/?post_type=projects&amp;path=FromNav">Architecture</a></li>
+												<li><a href="https://makezine.com/category/art-design/music/?post_type=projects&amp;path=FromNav">Music</a></li>
+												<li><a href="https://makezine.com/category/art-design/photography-video/?post_type=projects&amp;path=FromNav">Photography &amp; Video</a></li>
+												<li class="browse-all"><a href="https://makezine.com/category/art-design/?path=FromNav">❯ Browse All</a></li>
+											</ul>
+										</div>
+										<div class="span12 pull-right mega-nav-footer">
+											<a href="http://makezine.com/projects/">All Projects</a>
+											<a href="https://makezine.com/category/workshop/">Weekend Projects</a>
+										</div>
+
+									</ul>
+								</li>
+								<li id="nav-news" class="mega-box menu-item dropdown"><a href="#" class="dropdown-toggle">News</a>
+									<ul class="sub-menu dropdown-menu" style="width:940px;margin-left:-89px;">
+										<div class="span2">
+											<ul class="mega-dropdown">
+												<li class="top-cat-item"><a href="http://makezine.com/blog/?path=FromNav">All News</a></li>
+											</ul>
+										</div>
+										<div class="span2">
+											<ul class="mega-dropdown">
+												<li class="top-cat-item"><a href="https://makezine.com/category/maker-pro/?path=FromNav">Maker Pro</a></li>
+												<li><a href="https://makezine.com/category/maker-pro/open-source-hardware/?path=FromNav">Open Source Hardware</a></li>
+												<li><a href="https://makezine.com/category/maker-pro/makerspaces/?path=FromNav">Makerspaces</a></li>
+												<li><a href="https://makezine.com/category/maker-pro/crowdfunding/?path=FromNav">Crowdfunding</a></li>
+												<li><a href="https://makezine.com/maker-pro-newsletter/?path=FromNav">Maker Pro Newsletter</a></li>
+											</ul>
+										</div>
+										<div class="span2">
+											<ul class="mega-dropdown">
+												<li class="top-cat-item"><a href="http://makezine.com/tag/maker-faire/?path=FromNav">Maker Faire</a></li>
+											</ul>
+										</div>
+										<div class="span3">
+											<ul class="mega-dropdown">
+												<li class="top-cat-item"><a href="http://makezine.com/category/makers/?path=FromNav">Meet the Makers</a></li>
+											</ul>
+										</div>
+										<div class="span2">
+											<ul class="mega-dropdown">
+												<li class="top-cat-item"><a href="http://makezine.com/category/kids-family/?path=FromNav">Kids &amp; Family</a></li>
+											</ul>
+										</div>
+									</ul>
+								</li>
+								<li class="menu-item dropdown"><a href="#" class="dropdown-toggle">Videos</a>
+									<ul class="sub-menu dropdown-menu">
+										<li class="menu-item"><a href="https://makezine.com/video?path=FromNav">All Videos</a>
+									</ul>
+								</li>
+								<li class="menu-item dropdown"><a href="#" class="dropdown-toggle">Events</a>
+									<ul class="sub-menu dropdown-menu">
+										<li><a target="_blank" href="http://makerfaire.com/?path=FromNav">Maker Faire</a></li>
+										<li><a target="_blank" href="http://makercon.com?path=FromNav">MakerCon</a></li>
+										<li><a target="_blank" href="http://makercamp.com?path=FromNav">Maker Camp</a></li>
+									</ul>
+								</li>
+								<li class="menu-item dropdown"><a href="#">Shop</a>
+									<ul class="sub-menu dropdown-menu">
+										<li id="menu-item-318846" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-318846"><a target="_blank" href="http://www.makershed.com/?utm_source=makezine.com&amp;utm_medium=ads&amp;utm_campaign=Top+Nav+Menu&amp;utm_term=Maker+Shed+Store&amp;path=FromNav">Maker Shed Store</a></li>
+									</ul>
+								</li>
+								<li class="menu-item dropdown"><a href="#" class="dropdown-toggle">Publications</a>
+									<ul class="sub-menu dropdown-menu">
+										<li><a target="_blank" href="https://www.pubservice.com/MK/subscribe.aspx?PC=MK&PK=M3BMZA&amp;path=FromNav">Subscribe to Make:</a></li>
+										<li><a target="_blank" href="http://makezine.com/volume/make-43/?path=FromNav">Order Current Issue</a></li>
+										<li><a target="_blank" href="http://www.makershed.com/collections/make-magazine?path=FromNav">Order Back Issues</a></li>
+										<li><a target="_blank" href="http://make-digital.com/?path=FromNav">Digital Edition</a></li>
+										<li><a target="_blank" href="http://www.makershed.com/collections/books-magazines?path=FromNav">Buy Books</a></li>
+									</ul>
+								</li>
+							</ul>
 						</nav>
 					</div>
 				</div>
